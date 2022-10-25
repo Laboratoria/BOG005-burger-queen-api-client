4612e61346b7e64e20fd5619a2f403f30aae340c
jeniferSamper <jenifersamperusb@gmail.com>
2022-10-25 11:49:33 -0500

    avance de la vista login

--- burger-queen/src/components/home/login.jsx
+++ burger-queen/src/components/home/login.jsx
@@ -1,10 +1,22 @@
 import React from "react";
+import logoFondoPizarra from './images/logoSinFondo.png'
 
 
 const Login = () => {
     return (
         <div className="login_container">
-            <p>Pagina en construcción</p>
+            <header>
+                <img src={logoFondoPizarra} alt="logo" />
+            </header>
+            <div className="form_container">
+                <form>
+                    <label>Correo:</label>
+                    <input type='email'></input>
+                    <label>Contraseña:</label>
+                    <input type='password'></input>
+                </form>
+                <button>INICIAR SESIÓN</button>
+            </div>
         </div>
     )
 }
