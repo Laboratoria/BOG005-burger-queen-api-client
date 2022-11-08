e5f28bb69e236342ee08ff8f99095412b1298b83
jeniferSamper <jenifersamperusb@gmail.com>
2022-11-08 14:26:25 -0500

    cambios de prueba

--- burger-queen/src/view/admin.jsx
+++ burger-queen/src/view/admin.jsx
@@ -1,8 +1,10 @@
 import React from "react";
-// import logoFondoPizarra from '../img/logoSinFondo.png'
-import {AdminUser} from '../components/admin/adminUser/adminUser.jsx'
-import Navbar from "../components/navBar";
+import logoFondoPizarra from '../img/logoSinFondo.png'
+
+import {AdminUser} from '../components/admin/adminUser/adminUser'
 // import Navbar from "../components/navBar";
+// import Navbar from "../components/navBar";
+
 
 
 
@@ -10,16 +12,10 @@ const Admin= () => {
     return (
         <div className="admin_Container">
             <header>
-            {/* <img src={logoFondoPizarra} alt="logo" /> */}
-            {/* <nav>
+            <img src={logoFondoPizarra} alt="logo" />
+            <nav>
             <i className="fa-solid fa-bars"></i>
-            </nav> */}
-            <Navbar
-                item1="USUARIOS"
-                item2="PRODUCTOS"
-                link1="/admin"
-                link2="/admin-products"
-            />
+            </nav>
             </header>
             <AdminUser />
         </div>
