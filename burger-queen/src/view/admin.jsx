a063aab03c5c938d009db846ffd5f334a1b6f594
estefaniazocar <estefanyazocar3@gmail.com>
2022-11-08 13:41:30 -0500

    estilos de barra de navegacion

--- burger-queen/src/view/admin.jsx
+++ burger-queen/src/view/admin.jsx
@@ -1,7 +1,9 @@
 import React from "react";
 // import logoFondoPizarra from '../img/logoSinFondo.png'
 import {AdminUser} from '../components/admin/adminUser/adminUser.jsx'
-import Navbar from "../components/navBar";
+import { Navbar } from "../components/navBar.jsx";
+
+
 // import Navbar from "../components/navBar";
 
 
@@ -14,12 +16,13 @@ const Admin= () => {
             {/* <nav>
             <i className="fa-solid fa-bars"></i>
             </nav> */}
-            <Navbar
+            {/* <Navbar
                 item1="USUARIOS"
                 item2="PRODUCTOS"
                 link1="/admin"
                 link2="/admin-products"
-            />
+            /> */}
+            <Navbar/>
             </header>
             <AdminUser />
         </div>
