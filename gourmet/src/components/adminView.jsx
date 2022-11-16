987dd33cf053b8bc40cabbe97e46cd298791f9b8
KarenthCruz <spoonecologic@gmail.com>
2022-11-16 15:52:10 -0500

    Admin View

--- gourmet/src/components/adminView.jsx
+++ gourmet/src/components/adminView.jsx
@@ -1,1 +1,28 @@
-//vista de admin
+import React, { useState } from "react";
+
+<section className="adminView">
+
+    <section className="header">
+        <div className="imgHeader">
+        <img src="/burger-queen-logo.png" className="logoHeader" alt="Burger logo" />
+        <img src="/burger-queen-header.png" className="logoHeaderBig" alt="Burger logo" />
+        </div>
+
+        <nav className="menuNav"></nav>
+        {/* Menú de navegació en react */}
+
+    </section>
+
+    <section className="productsList">
+    {/* componente de productos */}
+    </section>
+
+    <section className="modalAddProd">   
+    {/* form para crear productos  */}
+    </section>
+
+    <section className="modalEditProd">   
+    {/* form para crear productos  */}
+    </section>
+
+</section>
