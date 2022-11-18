1f2d12260c6779b27720316382d0e405b51be5b3
jelimarl <jelimarlm@gmail.com>
2022-11-18 12:04:21 -0500

    Modal hook and component

---
+++ gourmet/src/components/addProductModal.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Modal } from './modal.jsx';
+
+function AddProductModal(props) {
+
+  return (
+    <Modal {...props} title='Hola'>
+
+      <h3>Veamos</h3>
+
+    </Modal>
+  )
+}
+
+export { AddProductModal }
--- gourmet/src/components/adminView.jsx
+++ gourmet/src/components/adminView.jsx
@@ -1,7 +1,31 @@
 import React, { useState } from "react";
+import { useModal } from "../hooks/useModal.js";
+import { AddProductModal } from "./addProductModal.jsx"
 
 function AdminView() {
-    <section className="adminView">
+
+    const [isOpenAddProductModal, openAddProductModal, closeAddProductModal] = useModal()
+
+    //const [isOpenEditProductModal, openEditProductModal, closeEditProductModal] = useModal()
+
+    return (
+        <div>
+            <button onClick={openAddProductModal}>
+                Agregar Productos
+            </button>
+
+            {/* <button onClick={openEditProductModal}>
+                Editar Productos
+            </button> */}
+            <AddProductModal
+                isOpen={isOpenAddProductModal}
+                closeModal={closeAddProductModal}
+            >
+            </AddProductModal>
+        </div>
+    );
+
+    < section className="adminView" >
 
         <section className="header">
             <div className="headerImg">
@@ -26,7 +50,7 @@ function AdminView() {
             {/* form para crear productos  */}
         </section>
 
-    </section>
+    </section >
 }
 
 export { AdminView }
