1873a259a81fcaff52fce640b4014fb1e9714f23
Adriana Ruiz <adriannaruiz24@gmail.com>
2022-11-25 11:42:10 -0500

    con ruteado funcionando

--- src/routes/RoutesByRole.js
+++ src/routes/RoutesByRole.js
@@ -1,20 +1,68 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 import Administration from './Administration'
+import Order from './Order'
+import OrderState from './OrderState'
+import { Products } from './Products'
+import { Users } from './Users'
 
 const RoutesByRole = () => {
 
     const userActived = localStorage.getItem('userRole')
 
+    console.log(userActived)
     return (
         <>
-            <Routes>
-                {
-                    userActived && (
-                        <Route path='/admin' element={<Administration />} />
+
+            {
+                userActived === 'admin'
+                    ? (
+                        <Routes>
+                            <Route path='/admin' element={< Administration />}></Route>
+                            <Route path='/order' element={<Order />}></Route>
+                            <Route path='/orderState' element={<OrderState />}></Route>
+                            <Route path='/users' element={<Users />}></Route>
+                            <Route path='/products' element={<Products />}></Route>
+                        </Routes>
                     )
-                }
-            </Routes>
+                    : (
+                        <Routes>
+                            <Route path='/admin' element={< Administration />}></Route>
+                        </Routes>
+                    )
+            }
+            {
+                userActived === 'Mesero'
+                    ? (
+                        <Routes>
+                            <Route path='/order' element={<Order />}></Route>
+                            <Route path='/orderState' element={<OrderState />}></Route>
+                        </Routes>
+
+                    )
+                    : (
+                        <Routes>
+                            <Route path='/order' element={<Order />}></Route>
+                        </Routes>
+
+                    )
+            }
+            {
+                userActived === 'Chef'
+                    ? (
+                        <Routes>
+                            <Route path='/orderState' element={<OrderState />}></Route>
+                        </Routes>
+
+                    )
+                    : (
+                        <Routes>
+                            <Route path='/orderState' element={<OrderState />}></Route>
+                        </Routes>
+
+                    )
+            }
+
         </>
     )
 }
