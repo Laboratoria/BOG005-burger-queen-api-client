fcb2e44cbe4622adb489877cfe2d4b5d5f5666ee
AlexaFuenmayor <estherf.m18@gmail.com>
2022-11-29 17:26:58 -0500

    completamos vista login, usando botstrao y cambiando algunos estilos en el css

--- src/routes/login.jsx
+++ src/routes/login.jsx
@@ -1,11 +1,33 @@
-import background from "../img/IMG-BACKGROUND.png";
+import logo from "../img/LOGO LOGO.png";
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
 import "./Login.css";
 export function Login() {
   return (
     <main>
       <div className="login">
+      <img src={logo} alt="logo" className="logo_imagen"/>
+        <section className="container_form">
+      
+        <Form className="login_form">
         <h1 className="login_title">Login</h1>
+      <Form.Group className="mb-3" controlId="formBasicEmail">
+      
+        <Form.Control type="email" placeholder="Correo electrónico" />
+      </Form.Group>
+
+      <Form.Group className="mb-3" controlId="formBasicPassword">
+
+        <Form.Control type="password" placeholder="Contraseña" />
+      </Form.Group>
+      <Button variant="primary" type="submit">
+        Ingresar
+      </Button>
+    </Form>
+    </section>
       </div>
+
+
     </main>
   );
 }
