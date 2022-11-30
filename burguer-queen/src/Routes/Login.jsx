f8fc7952a4aedaaa9c1da3428398787a5fae11f4
Aleja22LC <alejaleona@hotmail.com>
2022-11-30 12:46:16 -0500

    se sube la primera interaccion con style

--- burguer-queen/src/Routes/Login.jsx
+++ burguer-queen/src/Routes/Login.jsx
@@ -1,7 +1,63 @@
+import { useState } from "react";
+import "../App.css";
+import logop from "../img/logop.svg";
+
 export function Login() {
     return (
     <h1>
       Login
     </h1>
+    )
+  }
+
+function Button() {
+    return (
+      <button type='' className="btn">INGRESAR</button>
+    );
+  }
+
+  export default function MyForm() {
+    const [inputs, setInputs] = useState({});
+  
+    const handleChange = (event) => {
+      const name = event.target.name;
+      const value = event.target.value;
+      setInputs((values) => ({...values, [name]: value}))
+    }
+  
+    const handleSubmit = (event) => {
+      event.preventDefault();
+      console.log(inputs);
+    }
+  
+    return (
+      <form onSubmit={handleSubmit} className="login">
+        <img src={logop} alt='logo'/>
+        <div className="form-container">
+          <input 
+            type="email" 
+            placeholder="E-mail"
+            name="username" 
+            value={inputs.username || ""} 
+            onChange={handleChange}
+          />
+            <input 
+              type="password" 
+              placeholder="Contraseña"
+              name="pass" 
+              value={inputs.pass || ""} 
+              onChange={handleChange}
+            />
+            <div className="buttons-container">
+              <Button 
+              type="submit"
+              className="btn-start"
+              text="INGRESAR" 
+              />
+             
+            </div>
+        </div>
+          
+      </form>
     )
   }
--- burguer-queen/src/index.js
+++ burguer-queen/src/index.js
@@ -1,10 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
-import { Home } from "./Routes/Home";
+//import { Home } from "./Routes/Home";
 import { Users } from "./Routes/Users";
-import { Login } from "./Routes/Login";
+// import MyForm, { Login } from "./Routes/Login";
 import { Layout } from "./Layout";
+import App from "./Routes/Home";
+import MyForm from "./Routes/Login";
+
 //import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
@@ -12,12 +15,15 @@ root.render(
   <BrowserRouter>
     <Routes>
       <Route path="/" element={<Layout />}>
-        <Route index path="/" element={<Home />} />
+        <Route index path="/" element={<App/>} />
         <Route path="users" element={<Users />} />
-        <Route path="Login" element={<Login />} />
+        {/* <Route path="Login" element={<Login />} /> */}
+        <Route path="Login" element={<MyForm/>} />
         {/* <Route path="*" element={<div>404 - not found</div>} /> */}
         <Route path="*" element={<Navigate replace to="/" />} />
       </Route>
-    </Routes>
+    </Routes>  
+    
   </BrowserRouter>
+  
 );
