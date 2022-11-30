ae10c9d7f0413021daca8fb06faf0964ef1eeb0b
Katherine Cuartas Cardona <cuartascardonak@gmail.com>
2022-11-30 11:08:58 -0500

    integrando proyecto con react

---
+++ burgerqueen/src/Containers/Login.jsx
@@ -0,0 +1,14 @@
+
+import React from "react";
+import Form from "../Componentes/Form";
+
+
+ const Login = () => {
+     return (
+         <div>
+     <h1 className='tittle-login'> LOGIN </h1>
+             <Form />
+         </div>
+     )}
+
+export default Login;
