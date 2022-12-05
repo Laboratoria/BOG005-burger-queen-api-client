9503f27fdb5a58ef0043cdc498f68fffa1ad0454
AlexaFuenmayor <estherf.m18@gmail.com>
2022-12-05 16:32:57 -0500

    Terminamos la validación del login, ccreamos alertas para lo errores, y terminamos responsive para la vista de login (vista mobile y tablet)

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,4 +1,4 @@
-import logo from "../img/LOGO LOGO.png";
+import logo from "../img/FONDITO.png";
 // import loggo from "../img/FONDO-TABLET.png";
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
@@ -6,7 +6,9 @@ import "../Styles/Login.css";
 import React, { useState } from "react";
 import { useNavigate } from "react-router";
 import { order, orderDelivered } from "../routes/routes";
-import axios from 'axios'
+import axios from "axios";
+import Swal from "sweetalert2";
+// import swal from 'sweetalert';
 
 const url = "http://localhost:8080/login";
 
@@ -24,30 +26,48 @@ export function Login() {
   };
 
   const loginSesion = () => {
-    axios.post(url, {
+    axios
+      .post(url, {
         email: email,
         password: password,
         headers: {
           "content-type": "application/json",
-        }
+        },
       })
       .then((response) => {
+        console.log(response.data);
         const role = response.data.user.role;
         switch (role) {
-          case 'admin':
-            navigate(orderDelivered)
+          case "admin":
+            navigate(orderDelivered);
             break;
           case "mesero":
             navigate(order);
             break;
+          case "":
+            console.log("INGRESE SUS DATOS");
+            break;
           default:
-            console.log("Usuario no existe");
         }
         localStorage.setItem("Token", response.data.accessToken);
         localStorage.setItem("userId", response.data.user.id);
       })
       .catch((err) => {
-        console.log(err);
+        if (email === "" || password === "") {
+          Swal.fire({title:'¡Debes llenar ambos campos!',
+          confirmButtonColor:'#dd7a1e'})
+
+
+        } else {
+          Swal.fire({
+            
+            icon: "error",
+            title: "Oops...",
+            text: "¡Datos incorrectos!",
+            footer: "<p class='text-center'>Vuelve a intentarlo o <br> consulte con el <br> administrador </p>",
+            confirmButtonColor:'#dd7a1e',
+          })(err);
+        }
       });
   };
   const submit = (e) => {
@@ -72,9 +92,9 @@ export function Login() {
 
   return (
     <main>
+     
       <div className="login">
-        <img src={logo} alt="logo" className="logo_imagen" />
-        {/* <img src={loggo} alt="logo" className="loggo_imagen" /> */}
+         <img src={logo} alt="logo" className="logo_imagen" />
         <section className="container_form">
           <Form className="login_form" onSubmit={submit}>
             <h1 className="login_title">Login</h1>
