1d06566108e46a9a1ad9ec4cd73047a5877f936c
Astrid-Macana <lorenamakna21@gmail.com>
2022-12-06 18:36:33 -0500

    en vista pedido se adiciona tarjeta que incluye tutorial de uso

--- src/components/Order.jsx
+++ src/components/Order.jsx
@@ -5,7 +5,9 @@ import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import { useNavigate } from "react-router";
 import { breakfast, lounch } from "../routes/routes";
-
+import Card from "react-bootstrap/Card";
+import ListGroup from "react-bootstrap/ListGroup";
+import mesero from "../img/mesero.png"
 // import Button from "react-bootstrap/Button";
 // import React, { useState } from "react"
 
@@ -28,27 +30,30 @@ export function Order() {
           <Navbar.Collapse id="responsive-navbar-nav">
             <Nav>
               <Nav.Link onClick={() => navigate(breakfast)}>Desayuno</Nav.Link>
-              <Nav.Link  onClick={() => navigate(lounch)}>
-                Almuerzo
-              </Nav.Link>
+              <Nav.Link onClick={() => navigate(lounch)}>Almuerzo</Nav.Link>
             </Nav>
           </Navbar.Collapse>
         </Container>
       </Navbar>
-      {/* <nav>
-        <h1 className="title"> Pedido </h1>
-        <Button className="btn-cocina" variant="primary" type="submit" >
-          Desayuno
-        </Button>
-        <Button className="btn-cocina" variant="primary" type="submit" >
-          Almuerzo
-        </Button>
-        </nav> */}
       <Form.Control
         className="input-name"
         type="email"
         placeholder="Nombre del cliente"
       />
+      <Card  className="card">
+        <Card.Img src={mesero} alt="img-mesero" className="img-mesero"  />
+        <Card.Body>
+          <Card.Title>TUTORIAL</Card.Title>
+          <Card.Text className="text-cards">
+          Recuerda los pasos que debes seguir para hacer tu pedido
+          </Card.Text>
+        </Card.Body>
+        <ListGroup className="list-pasos">
+          <ListGroup.Item>1.Escriba el nombre del cliente</ListGroup.Item>
+          <ListGroup.Item>2.Despliegue las opciones</ListGroup.Item>
+          <ListGroup.Item>3.Selccione el menu que desee</ListGroup.Item>
+        </ListGroup>
+      </Card>
     </section>
   );
 }
