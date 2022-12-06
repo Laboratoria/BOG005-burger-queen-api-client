5fb633b626570d86123626a717c1f4c21b86b8af
alexandretta <alexsalazar21@gmail.com>
2022-12-05 20:02:51 -0500

    En esta rama de pruebas funciona una versión sencilla de CRUD para trabajar sobre él. aún no tiene funciones, estilos a medias... es sólo una prueba

---
+++ src/components/ProductsTable.jsx
@@ -0,0 +1,52 @@
+import React from "react";
+
+// // Componentes para los productos
+// const ProductTable = (product) => {
+//     return (
+//         <article className="productsContainer">
+//             {/* <img className="imageProduct" srcSet={product.image} alt={product.name} /> */}
+//             <div className="infoProduct">
+//                 <p>{product.id}</p>
+//                 <p>{product.name}</p>
+//                 <p>{product.price}</p>
+//                 <img srcSet={product.image} alt={product.name}></img>
+//                 <p>{product.type}</p>
+//             </div>
+//         </article>
+//     )
+// }
+
+// export { products }
+
+
+const ProductTable = (props) => (
+  <table>
+    <thead>
+      <tr>
+        <th>Producto</th>
+        <th>Precio</th>
+        <th>Acciones</th>
+      </tr>
+    </thead>
+    <tbody>
+    {props.products.length > 0 ? (
+        props.products.map((product) => (
+          <tr key={product.id}>
+            <td>{product.name}</td>
+            <td>{product.price}</td>
+            <td>
+              <button className="button muted-button">Edit</button>
+              <button className="button muted-button">Delete</button>
+            </td>
+          </tr>
+        ))
+      ) : (
+        <tr>
+          <td colSpan={3}>No hay productos</td>
+        </tr>
+      )}
+    </tbody>
+  </table>
+)
+
+export default ProductTable
--- src/components/Waiters.jsx
+++ src/components/Waiters.jsx
@@ -1,61 +1,74 @@
-import "./Waiters.css";
+import '../components/Waiters.css';
 import { useNavigate } from "react-router-dom";
 import { Link } from "react-router-dom";
-import { products } from "./products";
-
-const product = [ {
-    id: 1,
-    name: "Café Americano",
-    price: 5,
-    image:"../img/cafeamericano.jpeg",
-    type: "Desayuno",
-  },
-  {
-    id: 2,
-    name: "Café con leche",
-    price: 7,
-    image: "https://ibb.co/HXnKndy",
-    type: "Desayuno",
-  },
-  {
-    id: 3,
-    name: "Sandwich de jamón y queso",
-    price: 10,
-    image:"../img/sandwich.jpeg",
-    type: "Desayuno",
-  },
-  {
-    id: 4,
-    name: "Hamburguesa simple",
-    price: 10,
-    image:"../img/burger.png",
-    type: "Almuerzo",
-  },]
-
+import React, { useState } from "react";
+import ProductTable from "./ProductsTable";
 
 const Waiters = () => {
-    const navigate = useNavigate();
+  const productData = [
+    {
+      id: 1,
+      name: "Café Americano",
+      price: 5,
+      image: "../img/cafeamericano.jpeg",
+      type: "Desayuno",
+    },
+    {
+      id: 2,
+      name: "Café con leche",
+      price: 7,
+      image: "https://ibb.co/HXnKndy",
+      type: "Desayuno",
+    },
+    {
+      id: 3,
+      name: "Sandwich de jamón y queso",
+      price: 10,
+      image: "../img/sandwich.jpeg",
+      type: "Desayuno",
+    },
+    {
+      id: 4,
+      name: "Hamburguesa simple",
+      price: 10,
+      image: "../img/burger.png",
+      type: "Almuerzo",
+    },
+  ];
+
+  const navigate = useNavigate();
+
+  const [products, setProducts] = useState(productData);
 
   return (
-    <section className="waiterOrders">
-        <div className='linkWaiter'>
-            <Link className="linkOrders" to="/Waiters">Pedidos</Link>  {" "}
-            <Link className="linkStatus" to="/login">Estado</Link>
-        </div>
-            <div className="pedidos">
-                <h3 className="takeOrders">Tomando pedidos</h3>
-                <h4 className="orderNum">Nº de orden</h4>
-                <input className="orderNumber" type="number"></input>
-                <div className="buttonProducts">
-                    <p>{products(product[1])}</p> 
-                </div>
-            <div className="buttonsWaiter">
-        <button className="hola" onClick={() => navigate("/Login")}>REGRESAR</button>
-        <button className="hola" onClick={() => alert('Producto cargado')}>CONFIRMAR</button>
-        </div>
-        </div>
+    <section className='waiterOrders'>
+      <h1>BURGER QUEEN TELECOMANDA </h1>
+      <div className='linkWaiter'>
+        <Link className='linkOrders' to='/Waiters'>
+          Pedidos
+        </Link>{" "}
+        <Link className='linkStatus' to='/Waiters'>
+          Estado
+        </Link>
+      </div>
+      <div className='takeOrders'>
+        <h2>AGREGAR PRODUCTOS</h2>
+      </div>
+      <h4 className='orderNum'>Nº de orden</h4>
+      <input className='orderNumber' type='number'></input>
+      <div className='flex-large'>
+        <h2>View products</h2>
+        <ProductTable products={products} />
+      </div>
+      <div className='buttonsWaiter'>
+        <button className='hola' onClick={() => navigate("/Login")}>
+          REGRESAR
+        </button>
+        <button className='hola' onClick={() => alert("Producto cargado")}>
+          CONFIRMAR
+        </button>
+      </div>
     </section>
   );
-}
-
+};
 export default Waiters;
