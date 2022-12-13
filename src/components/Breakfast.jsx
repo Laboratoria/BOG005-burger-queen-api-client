1a3b069ff0f48822e29346205cb498cfcef93ef8
AlexaFuenmayor <estherf.m18@gmail.com>
2022-12-13 01:02:12 -0500

    empezamos renderizado de vista breakfast y lunch, pasamos la petion  a la API al componente breakfast,

--- src/components/Breakfast.jsx
+++ src/components/Breakfast.jsx
@@ -4,24 +4,15 @@ import Nav from "react-bootstrap/Nav";
 import icon from "../img/button-back.png";
 import axios from "axios";
 import React, { useEffect, useState } from "react";
+import {Link} from 'react-router-dom'
+import Container from "react-bootstrap/Container";
+import Navbar from "react-bootstrap/Navbar";
+
 
 
 export function Breakfast() {
   const url = "http://localhost:8080/products";
   const [products, setProducts] = useState([]);
-
-  function  Prueba(props){
-    const elementos = props.product
-    const itemsProducts = elementos.map((item) => (
-      <li key={item}> name:{item.name} tipo: {item.type}</li>)
-      
-      )
-      // console.log(itemsProducts[0])
-     
-
-  }
-  console.log(Prueba)
-
 
 
   useEffect(() => {
@@ -45,16 +36,26 @@ export function Breakfast() {
 
 
   return (
+
     <section className="order-breakfast">
+          <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+    <Container>
+      <Navbar.Brand href="">Pedido</Navbar.Brand>
+      <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+      <Navbar.Collapse id="responsive-navbar-nav">
+        <Nav>
+        <Nav.Link > <Link to="/lounch" >Almuerzo</Link></Nav.Link>
+        </Nav>
+      </Navbar.Collapse>
+    </Container>
+  </Navbar>
       <Nav className="nav">
         <Nav.Item>
-          <Nav.Link href="/order">
-            <img src={icon} alt="icon-back" className="icon-back" />
-          </Nav.Link>
+        <Nav.Link ><Link to="/order" ><img src={icon} alt="icon-back" className="icon-back" /></Link></Nav.Link>
         </Nav.Item>
-        <Nav.Item className="title-pedido">DESAYUNO</Nav.Item>
+        <h1 className="title-pedido">Desayuno</h1>
         <Nav.Item>
-          <Button variant="success">Enviar</Button>{" "}
+          <Button variant="success">ENVIAR</Button>{" "}
         </Nav.Item>
       </Nav>
       <main>
